8bec887b92ba93b0cdcab015740681f247af0648
danis <ahrnadeev@gmail.com>
2026-04-24 03:07:16 +0300

    feat: add task4

--- scripts/02-do-backup/task1/03-p-cron.sh
+++ scripts/02-do-backup/task1/03-p-cron.sh
@@ -1,1 +1,9 @@
-0 3 * * * ~/backup-script.sh
+touch backup-script.sh
+
+cat >> backup-script.sh << EOF
+# script
+EOF
+
+(crontab -l 2>/dev/null; echo "0 3 * * * ~/backup-script.sh") | crontab -
+
+# crontab -l
